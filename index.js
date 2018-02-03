8743c3cb46586baf76988513dec5be82114f5c0c
Haarni Atte J <atte.h.1998@gmail.com>
2018-02-03 12:41:39 +0200

    Teitojen lisäys ja lisäyksen tarkistaminen

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
   {
@@ -24,20 +27,53 @@ let persons = [
   }
 ]
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
-app.get('/api/persons/:id',(req, res) => {
+app.get('/info', (request, response) => {
+  response.send(
+    `<p>puhelinluettelossa ${persons.length} ihmisen tiedot</p>
+    <p>${new Date()}</p>`
+  )
+})
+
+app.get('/api/persons/:id', (request, response) => {
   console.log('toimii')
-  const id = Number(req.params.id)
+  const id = Number(request.params.id)
   const person = persons.find(p => p.id === id)
   if (person) {
-    res.json(person)
+    response.json(person)
   } else {
-    console.log('404')
-    res.status(404).end()
+    response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (body.name === undefined) {
+    return response.status(400).json({error: 'name missing'})
+  } else if (body.number === undefined) {
+    return response.status(400).json({error: 'number missing'})
+  }
+
+  if (persons.some(n => n.name === body.name)) {
+    return response.status(400).json({error: 'name must be unique'})
+  }
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * Math.floor(1000000))
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
